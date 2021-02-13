087953a79d0213b629b54019f58ac75757dfed47
KyronEpperson <epperson.kyron@gmail.com>
2021-02-13 16:51:24 -0600

    Nice
    Nice

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void collidedWithEnemy(Enemy enemy)
+    {
+        // Enemy attack code
+        enemy.Attack(this);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Enemy enemy = col.collider.gameObject.GetComponent<Enemy>();
+        collidedWithEnemy(enemy);
+    }
+}
