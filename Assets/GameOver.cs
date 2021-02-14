d4ab970a3c455cf7a784db08e9ee31d414b08ba3
KyronEpperson <epperson.kyron@gmail.com>
2021-02-14 13:15:55 -0600

    Minor tweaks and Game Over Scene

---
+++ Assets/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+    public static bool gameIsOver = false;
+
+    public void endGame()
+    {
+        if (gameIsOver == false)
+        {
+            gameIsOver = true;
+            Debug.Log("Game over");
+
+            //restart game
+            loadQuitScene();
+        }
+    }
+
+    void loadQuitScene ()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
